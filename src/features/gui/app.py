2f4e362f70970790a18ac98beac5dec41d4a28e5
jerem <dogeusdoge@gmail.com>
2026-07-09 00:04:54 +0200

    Adjust window height to 900px for improved layout consistency

--- src/features/gui/app.py
+++ src/features/gui/app.py
@@ -52,8 +52,8 @@ class MetronomeGUI(tk.Tk):
                 super().__init__()
 
                 self.title("BPM Metronome")
-                self.geometry("1280x720")
-                self.minsize(1280, 720)
+                self.geometry("1280x900")
+                self.minsize(1280, 900)
 
                 self.style = ttk.Style(self)
 
